f91002f02df2e6c3c150cf0a48da39d2f6101382
Yngwie2023 <oigor9667@gmail.com>
2025-04-02 15:44:37 +0300

    Загрузка ДЗ

---
+++ README.md
@@ -0,0 +1,5 @@
+# Домашнее задание № 6. Django Tutorial
+
+Курс "Python Developer. Professional"
+
+Пройденный туториал для Django 4.0, от [части 1](https://docs.djangoproject.com/en/4.0/intro/tutorial01/) до [части 7](https://docs.djangoproject.com/en/4.0/intro/tutorial07/)
---
+++ tutorial/polls/views.py
@@ -0,0 +1,47 @@
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+
+    try:
+        choice_selected = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        choice_selected.votes += 1
+        choice_selected.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
